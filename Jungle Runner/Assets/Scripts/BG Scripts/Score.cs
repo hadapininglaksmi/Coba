3eeb0ec9e986a3d86223a2ddfd709c88747c629f
hadapininglaksmi <purwanithami@gmail.com>
2019-12-20 07:33:50 +0700

    Tes Unity Project

---
+++ Jungle Runner/Assets/Scripts/BG Scripts/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private Text scoreText;
+    private int difficultyLevel = 1;
+    private int maxDifficultyLevel = 10;
+    private int scoreToNextLevel = 2;
+    private float score = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score >= scoreToNextLevel)
+            LevelUp();
+        score += Time.deltaTime * difficultyLevel;
+    
+    }
+
+
+    void LevelUp()
+    {
+        if (difficultyLevel == maxDifficultyLevel)
+            return;
+        scoreToNextLevel *= 2;
+        difficultyLevel++;
+
+        GetComponent<BGMover>().SetSpeed(difficultyLevel);
+
+        Debug.Log(difficultyLevel);
+    }
+
+
+
+}
---
+++ Jungle Runner/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Text highscoreText;
+
+     void Start()
+    {
+        highscoreText.text = "Highscore : " + (int)PlayerPrefs.GetFloat("ScoreText");
+    }
+
+
+    public void PlayGame() {
+		SceneManager.LoadScene ("Gameplay");
+	}
+}
